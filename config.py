73883fc628035b6a4190b8850bb4ca80167d2946
frez <rezende.f@outlook.com>
2024-02-06 23:39:38 +0800

    testing deplyment on Fl0

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
+"""Contains the Application Configuration Parameters"""
+# https://realpython.com/flask-blueprint/
+
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-url = os.getenv('FL0_URL')
+#url = os.getenv('FL0_URL')
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = db_url
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_APP = 'app.py'
+    FLASK_ENV = 'development'
